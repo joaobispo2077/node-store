72630fa14fc9bd05c4724f8c9ed89b50c5c3452c
João Bispo <vitor.vi10@hotmail.com>
2020-10-07 09:37:22 -0300

    :rocket: feat: basic CRUD routes

--- src/app.js
+++ src/app.js
@@ -4,6 +4,11 @@ const express = require('express');
 const app = express();
 const router = express.Router();
 
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: true
+}));
+
 const route = router.get('/', (req, res, next) => {
     res.status(200).send({
         title: "Node Store API",
@@ -11,8 +16,28 @@ const route = router.get('/', (req, res, next) => {
     })
 });
 
+const create = router.post('/', (req, res, next) => {
+    const body = req.body;
+    res.status(201).json(body);
+});
+
+const update = router.patch('/:id', (req, res, next) => {
+    const { id } = req.params;
+    const body = req.body;
+    res.status(200).json({ id: id, item: body });
+});
+
+
+const remove = router.delete('/:id', (req, res, next) => {
+    const { id } = req.params;
+    const body = req.body;
+    res.status(200).json({ id, item: body });
+});
 
 
 app.use('/', route);
+app.use('/products', create);
+app.use('/products', update);
+app.use('/products', remove);
 
 module.exports = app;
