28258112c5a5f127f792214aaa8d14d9e2f19851
João Bispo <vitor.vi10@hotmail.com>
2020-10-09 14:28:44 -0300

    :wrench: fix: connect to mongodb atlas

--- src/app.js
+++ src/app.js
@@ -1,7 +1,18 @@
 /* eslint-disable no-unused-vars */
 const express = require('express');
-const router = express.Router();
+const mongoose = require("mongoose");
+
 const app = express();
+const uri = 'mongodb://admin:admin@ndstr-shard-00-00.chsnt.azure.mongodb.net:27017,ndstr-shard-00-01.chsnt.azure.mongodb.net:27017,ndstr-shard-00-02.chsnt.azure.mongodb.net:27017/ndstr?ssl=true&replicaSet=atlas-1395iu-shard-0&authSource=admin&retryWrites=true&w=majority';
+
+// mongoose.connect(uri, {
+//     useNewUrlParser: true
+// });
+
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('MongoDB connected...'))
+    .catch(err => console.log(err + '\n \n could not connect!'));
+
 
 const indexRoutes = require('./routes/index');
 const productsRoutes = require('./routes/Products');
