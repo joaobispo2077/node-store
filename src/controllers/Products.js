644a8f1744561d9945c65ad2e15c3650f338db84
João Bispo <vitor.vi10@hotmail.com>
2020-10-12 12:58:56 -0300

    :rocket: feat: update and delete product by id

--- src/controllers/Products.js
+++ src/controllers/Products.js
@@ -97,12 +97,30 @@ exports.post = (req, res, next) => {
 }
 exports.patch = (req, res, next) => {
     const { id } = req.params;
-    const body = req.body;
-    res.status(200).json({ id: id, item: body });
+    const { title, description, price, slug } = req.body;
+
+    Product.findByIdAndUpdate(id, {
+        $set: {
+            title,
+            description,
+            price,
+            slug
+        }
+    }).then(data => {
+        res.status(201).json(data);
+
+    }).catch(err => {
+        res.status(400).json({ message: `Falha ao atualizar o produto ${title}`, error: err })
+    })
+
 };
 
 exports.delete = (req, res, next) => {
     const { id } = req.params;
-    const body = req.body;
-    res.status(200).json({ id, item: body });
+    Product.findOneAndRemove(id).then(data => {
+        res.status(200).json(data);
+
+    }).catch(err => {
+        res.status(400).json({ message: `Falha ao remover o produto de id: ${id}`, error: err })
+    })
 };
