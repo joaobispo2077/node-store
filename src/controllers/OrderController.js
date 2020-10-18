5617a1cd671dea16d4abbff189014e28065c4ffd
João Bispo <vitor.vi10@hotmail.com>
2020-10-18 17:24:48 -0300

    :rocket: feat: create order with order id

--- src/controllers/OrderController.js
+++ src/controllers/OrderController.js
@@ -1,21 +1,34 @@
 /* eslint-disable no-unused-vars */
 'use strict'
 
+const guid = require('guid');
+
 const ValidatorContract = require('../validators/fluidValidator');
 const repository = require('../repositories/orderRepository');
 
 
-exports.listAll = async () => {
-  return await repository.get();
+exports.listAll = async (req, res, next) => {
+  try {
+    const orders = await repository.get();
+    res.status(200).json(orders);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: 'internal server error'});
+  }
 }
 exports.post = async (req, res, next) => {
-  const body = req.body;
+  const body = {
+    customer: req.body.customer,
+    number: guid.raw().substring(0, 6),
+    items: req.body.items
+  };
+
+
   const contract = new ValidatorContract();
 
-  contract.hasMinLen(body.customer, 3, 'O Nome deve conter pelo menos 3 caracteres');
-  contract.hasMinLen(body.number, 'O Email inválido, tente o modelo: teste@teste.teste');
-  contract.hasMinLen(body.status, 3, 'A Senha deve conter pelo menos 3 caracteres');
-  contract.hasMinLen(body.status, 3, 'A Senha deve conter pelo menos 3 caracteres');
+  contract.hasMinLen(body.customer, 3, 'O cliente deve ter um id válido');
+
+
 
   if (!contract.isValid()) {
       res.status(500).send(contract.errors()).end();
@@ -23,7 +36,7 @@ exports.post = async (req, res, next) => {
   }
 
   try {
-    const orderCreated = repository.create(body);
+    const orderCreated = await repository.create(body);
     res.status(201).json(orderCreated);
   } catch (err) {
     console.log(err);
