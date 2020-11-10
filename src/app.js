17dec26696b724726817412056532227996fdf56
João Bispo <vitor.vi10@hotmail.com>
2020-11-10 10:31:23 -0300

    :paperclip: chore: add cors & prepare to deploy

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@
 require('dotenv/config');
 const express = require('express');
 const mongoose = require("mongoose");
-
+const cors = require("cors");
 const app = express();
 
 const config = require('../config/config');
@@ -27,10 +27,15 @@ mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
     .catch(err => console.log(err + '\n \n could not connect!'));
 
 
-app.use(express.json());
+app.use(express.json({
+    limit: '10mb'
+}));
+
 app.use(express.urlencoded({
     extended: true
 }));
+
+app.use(cors());
 
 app.use('/', indexRoutes);
 app.use('/products', productsRoutes);
